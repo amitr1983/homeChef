bd17e1af19dad1fdf206c8c937543dcbf218c71c
tranpham <pminhtran@gmail.com>
2017-10-27 17:45:19 -0700

    storyboard for chef app

---
+++ CookApp/HomeChefAds/HomeChefAds/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeChefAds
+//
+//  Created by Tran Pham on 10/27/17.
+//  Copyright © 2017 Tran Pham. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CookApp/HomeChefAds/HomeChefAds/SecondViewController.m
@@ -0,0 +1,29 @@
+//
+//  SecondViewController.m
+//  HomeChefAds
+//
+//  Created by Tran Pham on 10/27/17.
+//  Copyright © 2017 Tran Pham. All rights reserved.
+//
+
+#import "SecondViewController.h"
+
+@interface SecondViewController ()
+
+@end
+
+@implementation SecondViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
